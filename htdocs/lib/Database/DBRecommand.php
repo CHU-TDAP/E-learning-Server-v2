d371fa88a20366d2e12b3082ed39dbba6a9f0509
kobayashi <3011850@gmail.com>
2014-12-27 01:26:01 +0800

    add RecommandPoint and DBRecommand file

---
+++ htdocs/lib/Database/DBRecommand.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace UElearning\Database;
+
+use UElearning\Exception;
+
+require_once UELEARNING_LIB_ROOT.'/Database/Database.php';
+require_once UELEARNING_LIB_ROOT.'/Database/Exception.php';
+
+class DBRecomamnd extends Database
+{
+	public function __construct()
+	{
+		
+	}
+	
+	protected function queryBelongByWhere($where)
+	{
+		
+	}
+}
