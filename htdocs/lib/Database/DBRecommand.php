cc02589d2d6571a2833d4210acfe6291b53e0cc0
kobayashi <3011850@gmail.com>
2015-01-03 05:35:39 +0800

    完成DBRecommand類別且已測試完成

--- htdocs/lib/Database/DBRecommand.php
+++ htdocs/lib/Database/DBRecommand.php
@@ -2,12 +2,18 @@
 
 namespace UElearning\Database;
 
-use UElearning\Exception;
-
+require_once UELEARNING_ROOT.'/config.php';
 require_once UELEARNING_LIB_ROOT.'/Database/Database.php';
 require_once UELEARNING_LIB_ROOT.'/Database/Exception.php';
+use UElearning\Exception;
 
-class DBRecomamnd extends Database
+/**
+ * 查推薦學習點時所需要的表格資料
+ * Usage:
+ *
+ */
+
+class DBRecommand extends Database
 {
 
     /**
@@ -15,14 +21,14 @@ class DBRecomamnd extends Database
      * @param string $where SQL WHERE子句
      * @return array 查詢結果
      */
-	protected function queryBelongByWhere($where)
+	protected function queryEdgeByWhere($where)
 	{
 		$sqlString = "SELECT DISTINCT ".$this->table('Edge').".Ti, ".$this->table('Edge').".Tj, ".$this->table('Edge').".MoveTime".
                      " FROM ".$this->table('Edge')." WHERE ".$where;
-        $this->conndb->prepare($sqlString);
-        $this->conndb->execute();
+        $query = $this->connDB->prepare($sqlString);
+        $query->execute();
 
-        $queryAllResult = $this->conndb->fetchAll();
+        $queryAllResult = $query->fetchAll();
 
         if(count($queryAllResult) != 0)
         {
@@ -30,14 +36,51 @@ class DBRecomamnd extends Database
             foreach ($queryAllResult as $key => $thisResult)
             {
                 array_push($result,
-                    array("Ti" => $thisResult['Ti'],
-                          "Tj" => $thisResult['Tj'],
-                          "MoveTime" => $thisResult['MoveTime']));
+                    array("current_point" => $thisResult['Ti'],
+                          "next_point" => $thisResult['Tj'],
+                          "move_time" => $thisResult['MoveTime']));
             }
 
             return $result;
         }
         else return null;
 	}
+
+    protected function queryBelongByWhere($where)
+    {
+        $sqlString = "SELECT ".$this->table('tbelong').".Weights FROM ".$this->table('tbelong')." WHERE ".$where;
+        $query = $this->connDB->prepare($sqlString);
+        $query->execute();
+
+        $queryResult = $query->fetchAll();
+
+        if(count($queryResult) != 0)
+        {
+            $result = array();
+            foreach ($queryResult as $key => $thisResult)
+            {
+                array_push($result, array("weight" => $thisResult['Weights']));
+            }
+            return $result;
+        }
+        else return null;
+    }
+
+    public function queryBelongByID($next_point,$theme_number)
+    {
+        $whereClause = $this->table('tbelong').".thID = ".$this->connDB->quote($theme_number)." AND ".$this->table('tbelong').".TID = ".$this->connDB->quote($next_point);
+        $AllOfResult = $this->queryBelongByWhere($whereClause);
+
+        if(count($AllOfResult) != 0) return $AllOfResult;
+        else return null;
+    }
+
+
+    public function queryEdgeByID($currentPoint)
+    {
+        $AllOfResult = $this->queryBelongByWhere("Ti = ".$this->connDB->quote($currentPoint));
+        if(count($AllOfResult) != 0) return $AllOfResult;
+        else return null;
+    }
 
 }
